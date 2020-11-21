5938908fd674178a25cb40c081a42b7d3dd62482
Bobbi <soundwebvertising@gmail.com>
2020-11-21 08:33:19 -0800

    adding calculator files

---
+++ calculator.js
@@ -0,0 +1,34 @@
+
+
+function add (a, b) {
+	return a + b;
+}
+
+function subtract (a, b) {
+	
+}
+
+function sum (a ,b) {
+	
+}
+
+function multiply (a, b) {
+	
+}
+
+function power(a, b) {
+	
+}
+
+function factorial(a, b) {
+	
+}
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
