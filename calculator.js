e592f52d830329df20616b21e0206f496666f981
Bobbi <soundwebvertising@gmail.com>
2020-11-21 11:45:15 -0800

    calculator functions tested with jasmine

--- calculator.js
+++ calculator.js
@@ -1,27 +1,40 @@
-
-
-function add (a, b) {
-	return a + b;
+function add (x, y) {
+	return x + y;
 }
 
-function subtract (a, b) {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum (a ,b) {
-	
+//sum computes the sum of an array
+function sum (array) {
+	//array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+	return array = array.reduce(add, 0);
 }
 
-function multiply (a, b) {
-	
+function multiply (array) {
+	var sum = 1;
+    for (var i = 0; i < array.length; i++) {
+			sum = sum * array[i];
+		}
+		return sum;
 }
 
-function power(a, b) {
-	
+function power(x, y) {
+	let pow;
+	return pow = Math.pow(x, y);
 }
 
-function factorial(a, b) {
-	
+function factorial(x) {
+	let answer = 1;
+  if (x == 0 || x == 1){
+    return answer;
+  }else{
+    for(let i = x; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  } 
 }
 
 module.exports = {
---
+++ script.js
@@ -0,0 +1,25 @@
+clear (){
+
+}
+
+//delete a single digit
+delete(){
+
+}
+
+//append numbers to the screen when user clicks on them
+appendNumber() {
+
+}
+
+chooseOperand() {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+  
+}
